c7bf686be3b6c711c8558813faf3aa713ec565d1
jayshreebawankar <jayshreebawankar@gmail.com>
2022-12-28 22:50:36 +0530

    first commit

---
+++ routes/userRoute.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { registerUser, loginUser, logoutUser, getUserById, getAllUsers, loginStatus, updateUser, changePassword, forgotPassword } = require('../controllers/userController.js');
+const { Protect } = require('../middlewares/authMiddleware.js');
+
+router.post('/register', registerUser)
+router.post('/login', loginUser)
+router.get('/logout', logoutUser)
+router.get('/', getAllUsers)
+router.get('/getuser', Protect, getUserById)
+router.get('/loggedin', loginStatus)
+router.patch('/update', Protect, updateUser)
+router.patch('/updatepassword', Protect, changePassword)
+router.put('/forgotpassword', Protect, forgotPassword)
+
+module.exports = router;

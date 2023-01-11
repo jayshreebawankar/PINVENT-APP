d38cec255272186f1aab4c4cb92aa27a8a75a871
jayshreebawankar <jayshreebawankar27@gmail.com>
2023-01-11 19:33:16 +0530

    push

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,7 +8,7 @@ router.post('/login', loginUser)
 router.get('/logout', logoutUser)
 router.get('/', getAllUsers)
 router.get('/getuser', Protect, getUserById)
-router.get('/loggedin', loginStatus)
+router.get('/loggedin', loginStatus)x
 router.patch('/update', Protect, updateUser)
 router.patch('/updatepassword', Protect, changePassword)
 router.put('/forgotpassword', Protect, forgotPassword)
